b653be3e5474fdafe21f12f2d4de9c9b5781455e
Maheshsp3333 <126386813+Maheshsp3333@users.noreply.github.com>
2023-03-31 08:35:17 +0530

    update initial

---
+++ JS/index.js
@@ -0,0 +1,45 @@
+document.getElementById('icon').addEventListener('click', function () {
+    var nav = document.getElementsByTagName('nav')[0];
+    if (nav.style.display == 'block') {
+        nav.style.display = 'none';
+    } else {
+        nav.style.display = 'block';
+    }
+}, false);
+
+// ============ Sliding images JS =================================//
+
+var currentIndex = 1;
+      
+//Show current image
+showSlides(currentIndex);
+
+//Function to move Next
+function plusSlides(n) {
+   showSlides(currentIndex += n);
+}
+
+//Function to move back
+function currentSlide(n) {
+   showSlides(currentIndex = n);
+}
+
+
+//Initiate moving of slides
+function showSlides(n) {
+   var i;
+   var slides = document.getElementsByClassName("images");
+   var dots = document.getElementsByClassName("navigation-dot");
+   if (n > slides.length) {currentIndex = 1}
+   if (n < 1) {currentIndex = slides.length}
+   for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+   }
+   for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+   }
+   slides[currentIndex-1].style.display = "block";
+   dots[currentIndex-1].className += " active";
+}
+
+// =================Sliding Images JS End ==========================//
