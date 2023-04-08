005cf977e2f0b687fc72c5c4088c64a9b8c4a0fd
Maheshsp3333 <126386813+Maheshsp3333@users.noreply.github.com>
2023-04-08 10:59:48 +0530

    update responsive

--- JS/wishlist.js
+++ JS/wishlist.js
@@ -31,3 +31,21 @@ window.onclick = function (event) {
     }
   }
 };
+// ============= Drop Down Button 2  ==========================
+function myFunction1() {
+  document.getElementById("myDropdown1").classList.toggle("show1");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn1")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content1");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show1")) {
+        openDropdown.classList.remove("show1");
+      }
+    }
+  }
+};
