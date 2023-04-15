1631b0afb59804e3555e65a6c1d68dd4dbfefe64
Maheshsp3333 <126386813+Maheshsp3333@users.noreply.github.com>
2023-04-15 13:27:06 +0530

    udt

--- JS/login.js
+++ JS/login.js
@@ -74,7 +74,7 @@ function signingoogle() {
     // The signed-in user info.
     const user = result.user;
     // IdP data available using getAdditionalUserInfo(result)
-    // ...
+    window.location.href = "/HTML/login.html";
   }).catch((error) => {
     // Handle Errors here.
     const errorCode = error.code;
