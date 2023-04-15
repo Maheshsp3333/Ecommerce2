f5663600effa66524b825c7ae3518ddbc7cc7ec4
Maheshsp3333 <126386813+Maheshsp3333@users.noreply.github.com>
2023-04-15 15:17:38 +0530

    c

--- JS/login.js
+++ JS/login.js
@@ -86,14 +86,12 @@ function signingoogle() {
   });
   }
 
-
-
-
+// =========================== Continue With Facebook ===============//
 
 const password_show2 = document.getElementById("password-show");
 password_show2.addEventListener("click", myFunctionps2);
 function myFunctionps2() {
-  var y = document.getElementById("psw");
+  var y = document.getElementById("psw-r");
   if (y.type === "password") {
     y.type = "text";
   } else {
